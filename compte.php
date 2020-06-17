e91f783003f90df71b19cd133a9786a27e991ccc
naainia yousef <y.naainia@gmail.com>
2020-06-17 06:10:18 +0000

    pcommit

---
+++ compte.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>compte</title>
+</head>
+<body>
+l;cl,l,fdls,
+	<table>
+		<td>
+
+			<form action="login" method="POST">
+		
+     				<tr><input type="text" name="nom"></tr>
+     				<tr><input type="text" name="prenom"></tr>
+     				<tr><input type="text" name="telephone"></tr>
+     				<tr><input type="text" name="telephone"></tr>
+					 <tr> <input type="radio" id="male" name="gender" value="male">
+  						<label for="male">Male</label><br>
+				 		<input type="radio" id="female" name="gender" value="female">
+				  		<label for="female">Female</label><br>
+						<input type="radio" id="other" name="gender" value="other">
+						<label for="other">Other</label>	</tr>
+							
+
+     				
+
+
+			</form>
+
+		</td>
+
+	</table>	
+
+<?php
+
+
+
+
+?>
+</body>
+</html>
